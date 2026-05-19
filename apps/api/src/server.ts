a1e996f462459e5a0b1620488cf998aaa2f64f11
jonyprono <carlossoglo60@gmail.com>
2026-05-19 12:22:41 +0200

    Update server.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,13 +1,9 @@
 import Fastify from 'fastify'
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import dotenv from 'dotenv'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import { join } from 'path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-dotenv.config({ path: join(__dirname, '../.env') })
+dotenv.config()
 
 const app = Fastify({
   logger: {
